614da7e96809903fd29637759bedfeaafa066eb5
keven carvalho <dangerdasedicoes@gmail.com>
2024-01-20 01:48:43 -0300

    upd

--- base/models.py
+++ base/models.py
@@ -1,2 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
+class user(models.Model):
+    los_hannah_username = models.CharField(max_length=50)
+    los_hannah_email = models.EmailField(default=0)
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from base.views import index
+from django.urls import include, path
 
 urlpatterns = [
+    path('base/', include("base.urls")),
     path('admin/', admin.site.urls),
-    path('', index),
 ]
